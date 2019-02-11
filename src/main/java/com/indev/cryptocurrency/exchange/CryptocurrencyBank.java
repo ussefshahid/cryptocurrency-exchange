f02acef88fa256f9970bf1d1854323d09dc16617
Youssef TAMAHMACHTE <ytamahmachte@sqli.com>
2019-02-11 14:52:02 +0100

    the last commit

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -12,13 +12,13 @@ public class CryptocurrencyBank {
         supportedCryptoCurrencies.add(bitcoin);
     }
 
-    public int requestTransaction(Customer buyerCustomer, int sold, String bitcoin) {
-        if(!isSellerExist(bitcoin) || !getSeller(bitcoin).isSameCryptoCurrency(bitcoin)) return 0;
+    public int requestTransaction(Customer buyerCustomer, int sold, String cyptoCurrency) {
+        if(!isSellerExist(cyptoCurrency) || !getSeller(cyptoCurrency).isSameCryptoCurrency(cyptoCurrency)) return 0;
         increaseCustomersNumber();
 
-        getSeller(bitcoin).sellCrypto(sold, metcalfeLaw());
+        getSeller(cyptoCurrency).sellCrypto(sold, metcalfeLaw());
 
-        buyerCustomer.withCryptocurrency(bitcoin, sold);
+        buyerCustomer.withCryptocurrency(cyptoCurrency, sold);
         buyerCustomer.decreaseBalance(sold, metcalfeLaw());
         return sold;
     }
@@ -27,16 +27,16 @@ public class CryptocurrencyBank {
         sellerCustomers.add(sellerCustomer);
     }
 
-    public boolean isSellerExist(String bitcoin){
+    public boolean isSellerExist(String cyptoCurrency){
         return sellerCustomers.stream()
-                .filter(customer -> customer.isSameCryptoCurrency(bitcoin))
+                .filter(customer -> customer.isSameCryptoCurrency(cyptoCurrency))
                 .findFirst()
                 .orElse(null) != null;
     }
 
-    public Customer getSeller(String bitcoin){
+    public Customer getSeller(String cyptoCurrency){
         return sellerCustomers.stream()
-                .filter(customer -> customer.isSameCryptoCurrency(bitcoin))
+                .filter(customer -> customer.isSameCryptoCurrency(cyptoCurrency))
                 .findFirst()
                 .orElse(null);
     }
