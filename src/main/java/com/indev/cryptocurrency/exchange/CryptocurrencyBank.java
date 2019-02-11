95610941f32ad48f2ff1590d2634f1667853b643
Youssef TAMAHMACHTE <ytamahmachte@sqli.com>
2019-02-11 11:31:44 +0100

    remains the last test

--- src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
+++ src/main/java/com/indev/cryptocurrency/exchange/CryptocurrencyBank.java
@@ -1,14 +1,42 @@
 package com.indev.cryptocurrency.exchange;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptocurrencyBank {
+    private List<String> supportedCryptoCurrencies=new ArrayList<>();
+    private Customer sellerCustomer;
+    private int customersNumber=0;
+
     public void addSupportedCryptoCurrency(String bitcoin) {
-        
+        supportedCryptoCurrencies.add(bitcoin);
     }
 
-    public int requestTransaction(Customer buyerCustomer, int i, String bitcoin) {
-        return 0;
+    public int requestTransaction(Customer buyerCustomer, int sold, String bitcoin) {
+        if(!isSellerExist() || !sellerCustomer.isSameCryptoCurrency(bitcoin)) return 0;
+        increaseCustomersNumber();
+
+        sellerCustomer.sellCrypto(sold, metcalfeLaw());
+
+        buyerCustomer.withCryptocurrency(bitcoin, sold);
+        buyerCustomer.decreaseBalance(sold, metcalfeLaw());
+        return sold;
     }
 
     public void addSeller(Customer sellerCustomer) {
+        this.sellerCustomer=sellerCustomer;
+    }
+
+    public boolean isSellerExist(){
+        return sellerCustomer!=null;
+    }
+
+    private void increaseCustomersNumber(){
+        customersNumber++;
+    }
+
+    public int metcalfeLaw(){
+        if(customersNumber==1) return 1;
+        return customersNumber*2-customersNumber;
     }
 }
--- src/main/java/com/indev/cryptocurrency/exchange/Customer.java
+++ src/main/java/com/indev/cryptocurrency/exchange/Customer.java
@@ -1,11 +1,49 @@
 package com.indev.cryptocurrency.exchange;
 
 public class Customer {
-    public Customer withCryptocurrency(String bitcoin, int i) {
-        return null;
+    private String cryptoCurrency;
+    private int balance;
+    private int sold;
+
+    public Customer withCryptocurrency(String cryptoCurrency, int sold) {
+        this.cryptoCurrency=cryptoCurrency;
+        this.sold=sold;
+        return this;
     }
 
-    public Customer withBalance(int i) {
-        return null;
+    public Customer withBalance(int balance) {
+        this.balance=balance;
+        return this;
+    }
+
+    public void addBalance(int balance, int _x){
+        this.balance+=balance*_x;
+    }
+
+    public void decreaseBalance(int balance, int _x){
+        this.balance-=balance;
+    }
+
+    public void addSold(int sold, int _x){
+        this.sold+=sold*_x;
+    }
+
+    public void decreaseSold(int sold){
+        this.sold-=sold;
+    }
+
+    public void sellCrypto(int sold, int _x){
+        addBalance(sold, _x);
+        decreaseSold(sold);
+    }
+
+    public boolean isSameCryptoCurrency(String cyptoCurrency){
+        return this.cryptoCurrency.equals(cyptoCurrency);
+    }
+
+    @Override
+    public String toString() {
+        String print=+balance+":$";
+        return print+=(sold>0)? ","+sold+":"+cryptoCurrency: "";
     }
 }
